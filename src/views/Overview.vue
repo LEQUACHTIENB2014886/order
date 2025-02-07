3d3cb313d77a66c6674d8204b65f2b9fa0df61ba
ithiez <vovanthien1002@gmail.com>
2025-02-07 10:14:25 +0700

    overviews

--- src/views/Overview.vue
+++ src/views/Overview.vue
@@ -1,11 +1,47 @@
 <template>
+  <el-container>
     <div>
-      <h1>Giới thiệu</h1>
-      <p>Chào mừng bạn đến với Giới thiệu!</p>
+      <el-image src="/src/assets/aboutus/bg-aboutus-1.png" fit="cover" />
     </div>
-  </template>
-   <style scoped>
-   p{
-      margin-bottom: 700px;
-    }
-    </style>
+
+    <div class="wpb_wrapper">
+      <div class="wpb_text_column wpb_content_element ">
+        <div class="wpb_wrapper">
+          <h2>
+            <strong>
+              <span style="color: #ff6600;">ĐI ĐẦU TRONG VIỆC KẾ THỪA HƯƠNG VỊ TRÀ TRUYỀN THỐNG</span>
+            </strong>
+          </h2>
+          <p style="text-align: justify;">
+            <span style="font-size: 16px;">Vào năm 2019, nhằm truyền bá hương vị thơm ngon của trà Đài Loan, Hồng Trà
+              Ngô Gia Đài Loan đã có mặt tại khu vực miền Nam Việt Nam. Vì là loại trà thơm ngon, tự nhiên và tốt cho
+              sức khỏe nên Hồng Trà Ngô Gia rất được ưa chuộng và nhanh chóng nổi tiếng. Đến nay đã có gần 60 cửa hàng
+              trên cả nước , trở thành đơn vị đi đầu trong việc kế thừa hương vị trà truyền thống.
+            </span>
+          </p>
+        </div>
+      </div>
+    </div>
+  </el-container>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.el-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+}
+
+.wpb_wrapper {
+  background-color: lemonchiffon;
+  margin-top: 20px;
+  margin-right: calc(20% - 20px);
+  padding: 20px;
+  padding-right: 300px;
+}
+</style>
